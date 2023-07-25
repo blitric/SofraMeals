6c4067a9788a6398d50df79ea652bb9381ea0d7d
Alma <alma.blitric@gmail.com>
2023-07-25 00:36:43 -0500

    Fixed accessing instructions

--- MealPlannerBackend/src/main/java/com/soframeals/MealPlannerBackend/controllers/ApiController.java
+++ MealPlannerBackend/src/main/java/com/soframeals/MealPlannerBackend/controllers/ApiController.java
@@ -1,9 +1,6 @@
 package com.soframeals.MealPlannerBackend.controllers;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,21 +15,26 @@ public class ApiController {
     @Value("${apiKey}")
     String app_key;
 
+    @Value("${uriTest}")
+    String uri;
+
     @GetMapping("/extapi")
     public ResponseEntity<String> getApi() {
 
-        //String type = "public";
+        // String type = "public";
         // String q = "cheese";
-        String uri = "https://api.spoonacular.com/recipes/605537/information";
 
         RestTemplate restTemplate = new RestTemplate();
+
         HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<?> entity = new HttpEntity<>(requestHeaders);
+
         LinkedMultiValueMap<String, String> allRequestParams = new LinkedMultiValueMap<>();
         allRequestParams.add("apiKey", app_key);
         //allRequestParams.add("type", type);
         //allRequestParams.add("q", q);
 
-        HttpEntity<?> entity = new HttpEntity<>(requestHeaders);
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(uri).queryParams(allRequestParams);
         UriComponents uriComponents = builder.build().encode();
         ResponseEntity<String> responseEntity = restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, entity, String.class);
